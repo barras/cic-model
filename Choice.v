02d146480871baefb2d57690f0715ea3e5cf7171
barras <barras@7b0040dd-6dd6-45fd-888d-7922243e4794>
2012-02-27 01:43:58 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coqcert/trunk/cic-model@155 7b0040dd-6dd6-45fd-888d-7922243e4794

--- Choice.v
+++ Choice.v
@@ -9,5 +9,5 @@ Axiom choice_axiom : forall A B, choice A B.
 Definition unique_choice A B (E:B->B->Prop) :=
   forall (R:A->B->Prop),
   (forall x:A, exists y:B, R x y) ->
-  (forall x y y', R x y -> R x y' -> E y y') ->
+  (forall x y y', R x y -> (R x y' <-> E y y')) ->
   exists f:A->B, forall x:A, R x (f x).
