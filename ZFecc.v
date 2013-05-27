f7dd383f0169e1f9aca2a96c95da30eb0d7fc5b5
barras <barras@7b0040dd-6dd6-45fd-888d-7922243e4794>
2013-05-27 11:57:15 +0000

    improved proof of SN for ECC
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svn/coqcert/trunk/cic-model@189 7b0040dd-6dd6-45fd-888d-7922243e4794

--- ZFecc.v
+++ ZFecc.v
@@ -13,6 +13,7 @@ Fixpoint ecc (n:nat) : set :=
 Lemma ecc_grot : forall n, grot_univ (ecc n).
 destruct n; apply (grot_succ_typ gr).
 Qed.
+Hint Resolve ecc_grot.
 
 Lemma ecc_in1 : forall n, props ∈ ecc n.
 induction n; simpl; intros.
@@ -36,6 +37,12 @@ apply G_trans with (ecc n); trivial.
  apply ecc_in2.
 Qed.
 
+Lemma ecc_incl_le x m n :
+  (m <= n)%nat -> x ∈ ecc m -> x ∈ ecc n.
+induction 1; intros; auto with *.
+apply ecc_incl; auto.
+Qed.
+
 Lemma ecc_incl_prop : forall x, x ∈ props -> x ∈ ecc 0.
 simpl; intros.
 apply G_trans with props; trivial.
@@ -52,7 +59,6 @@ Lemma ecc_prod : forall n X Y,
   cc_prod X Y ∈ ecc n.
 intros.
 apply G_cc_prod; trivial.
-apply ecc_grot.
 Qed.
 
 Lemma ecc_prod2 : forall n X Y,
@@ -62,11 +68,85 @@ Lemma ecc_prod2 : forall n X Y,
   cc_prod X Y ∈ ecc n.
 intros.
 apply G_cc_prod; trivial.
- apply ecc_grot.
-
- apply G_trans with props; trivial.
-  apply ecc_grot.
-
-  apply ecc_in1.
+apply G_trans with props; trivial.
+apply ecc_in1.
 Qed.
 
+(* *)
+
+Lemma empty_in_ecc n : empty ∈ ecc n.
+apply G_trans with ZFcoc.props; auto.
+apply ecc_in1.
+Qed.
+
+Lemma one_in_ecc n : singl empty ∈ ecc n.
+apply G_trans with ZFcoc.props; auto.
+apply ecc_in1.
+Qed.
+
+(* ecc 0 is the set of hereditarily finite sets, so we need to skip it. *)
+Lemma omega_incl_ecc n : ZFord.omega ⊆ ecc n.
+red; intros.
+unfold ZFord.omega, ZFord.ord_sup in H.
+rewrite sup_ax in H.
+ destruct H.
+ rewrite ZFrepl.uchoice_ax in H0.
+  destruct H0.
+  destruct H0.
+  rewrite <- H2 in H1.
+  apply G_trans with (ZFord.nat2ordset x1); trivial.
+  elim x1; intros.
+   apply G_incl with ZFcoc.props; trivial.
+    apply ecc_in1.
+    red; intros.
+    apply empty_ax in H3; contradiction.
+
+   apply G_subset; trivial.
+   apply G_power; auto.
+
+  (* Fch *)
+  split;[|split]; intros.
+ revert H2; apply ex2_morph; red; intros; auto with *.
+ rewrite H1; reflexivity.
+
+ elim H using ZFnats.N_ind; intros.
+  revert H3; apply ex_morph.
+  red; intros.
+  apply ex2_morph; red; intros; auto with *.
+  rewrite H2; reflexivity.
+
+  exists (ZFord.nat2ordset 0); exists 0; simpl; auto with *.
+
+  destruct H2 as (y,(m,?,?)).
+  exists (ZFord.nat2ordset (S m)); exists (S m); simpl; auto with *.
+  apply ZFnats.succ_morph; trivial.
+
+ destruct H1; destruct H2.
+ rewrite <- H3; rewrite <- H4; rewrite H1 in H2; apply ZFnats.nat2set_inj in H2.
+ rewrite H2; reflexivity.
+
+ (* Fm *)
+do 2 red; intros.
+apply ZFrepl.uchoice_morph_raw.
+red; intros.
+apply ex2_morph.
+ red; intros.
+ rewrite H1; reflexivity.
+
+ red; intros.
+ rewrite H2; reflexivity.
+Qed.
+
+Lemma omega_in_ecc n : ZFord.omega ∈ ecc (S n).
+apply G_incl with (ecc n); auto.
+ apply grot_succ_in; exact gr.
+
+ apply omega_incl_ecc.
+Qed.
+
+Lemma N_in_ecc n : ZFnats.N ∈ ecc (S n).
+apply G_N; trivial.
+apply omega_in_ecc.
+Qed.
+
+Hint Resolve empty_in_ecc one_in_ecc omega_in_ecc N_in_ecc.
